ca662a86bd6d27ca5506383cd8a6da9ef25b7ed2
domenico <domenico@element.in>
2021-01-08 19:12:42 +0100

    create schema DDL

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS chat_schema
+    AUTHORIZATION postgres;
+
+CREATE SEQUENCE IF NOT EXISTS chat_schema.hibernate_sequence
+    INCREMENT 1
+    START 1
+    MINVALUE 0
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE chat_schema.hibernate_sequence
+    OWNER TO postgres;
+
+CREATE TABLE IF NOT EXISTS "chat_schema"."message" (
+    id BIGINT,
+    sender_name varchar,
+    text varchar,
+    send_date varchar
+);
+
