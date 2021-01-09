a4b8ac2703c0e146ce857f007c0104fba8febe57
domenico <domenico@element.in>
2021-01-09 11:59:59 +0100

    Add app component

---
+++ message-ui/src/app/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MessageDisplayer from "./components/MessageDisplayer";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">Doodle Chat App</header>
+        <MessageDisplayer />
+      </div>
+    </>
+  );
+}
+
+export default App;
